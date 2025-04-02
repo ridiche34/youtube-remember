ab59c5a3a2b1e68216cfdf8cc621fe25cf4f823b
ridiche <tudo.diac@gmail.com>
2025-04-02 16:20:07 +0300

    Disabled functionality on livestreams

--- youtube.js
+++ youtube.js
@@ -6,43 +6,51 @@ let videoElementExists = false;
 
 // Function to handle the presence or absence of the element
 function handleVideoElementPresence() {
-    const videoElement = document.querySelector('.html5-main-video');
+	const videoElement = document.querySelector('.html5-main-video');
 
-    if (videoElement != videoElementExists && videoElement) {
-        videoElementExists = videoElement;
-        console.log('Video appeared', videoElement);
+	if (videoElement != videoElementExists && videoElement) {
+		videoElementExists = videoElement;
+		console.log('Video appeared', videoElement);
 		videoElement.addEventListener('pause', () => {
-			console.log("Video paused")
-			if (window.location.href.includes("youtube.com/watch") ) {
-				const url = new URL(window.location.href);
-
-    			// Set the time parameter (in seconds)
-    			const yourTimeInSeconds = Math.floor(videoElement.currentTime);
-                if (yourTimeInSeconds > 5)
-                {
-                    url.searchParams.set('t', yourTimeInSeconds);
-                    console.log("New URL for current time:", url)
-    
-                    // Update the page URL without reloading
-                    window.history.replaceState({}, '', url);
-                }
+			console.log("Video paused");
+			const livebadge = document.getElementsByClassName("ytp-live-badge");
+			if (livebadge.length == 0 || window.getComputedStyle(livebadge[0]).display == "none")
+			{
+				if (window.location.href.includes("youtube.com/watch") ) {
+					const url = new URL(window.location.href);
+	
+					// Set the time parameter (in seconds)
+					const yourTimeInSeconds = Math.floor(videoElement.currentTime);
+					if (yourTimeInSeconds > 5)
+					{
+						url.searchParams.set('t', yourTimeInSeconds);
+						console.log("New URL for current time:", url);
+		
+						// Update the page URL without reloading
+						window.history.replaceState({}, '', url);
+					}
+				}
+			}
+			else
+			{
+				console.log("Not saving time; Is livestream");
 			}
 		})
-        // Additional actions when the element appears
-    } else if (videoElement != videoElementExists) {
-        videoElementExists = videoElement;
-        console.log('Video disappeared');
-        // Additional actions when the element disappears
-    }
+		// Additional actions when the element appears
+	} else if (videoElement != videoElementExists) {
+		videoElementExists = videoElement;
+		console.log('Video disappeared');
+		// Additional actions when the element disappears
+	}
 }
 
 // Create a callback for mutations, only reacting to the appearance/disappearance of the element
 const observer = new MutationObserver((mutationsList) => {
-    for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            handleVideoElementPresence();
-        }
-    }
+	for (let mutation of mutationsList) {
+		if (mutation.type === 'childList') {
+			handleVideoElementPresence();
+		}
+	}
 });
 
 // Start observing
